dc3718f7c7e85995724d60c4fd2ed0c78e662094
vaibhavmali2001 <vaibhavmali2387@gmail.com>
2025-05-26 16:56:26 +0530

    xyz

--- global_robotics_backend/global_robotics/src/main/java/com/site/Config/WebConfig.java
+++ global_robotics_backend/global_robotics/src/main/java/com/site/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")
-	                        .allowedOrigins("http://localhost:3000") // put frontend url
+	                        .allowedOrigins("https://unique-valkyrie-50c50d.netlify.app/") // put frontend url
 	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                        .allowedHeaders("*")
 	                        .allowCredentials(true);
