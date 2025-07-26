a470e263d7a7c9d2e4c5504813a3387ab3ee01d1
vaibhavmali2001 <vaibhavmali2387@gmail.com>
2025-07-26 16:16:56 +0530

    abc

--- global_robotics_backend/global_robotics/src/main/java/com/site/Config/WebConfig.java
+++ global_robotics_backend/global_robotics/src/main/java/com/site/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")
-	                        .allowedOrigins("http://localhost:3000") // put frontend url
+	                        .allowedOrigins("http://globalroboticsai.co.in") // put frontend url
 	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                        .allowedHeaders("*")
 	                        .allowCredentials(true);
