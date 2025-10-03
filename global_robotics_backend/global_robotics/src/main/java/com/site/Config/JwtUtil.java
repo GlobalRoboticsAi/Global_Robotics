066997f24611ea7e5c87bef92dbfdda50ab80675
vaibhavmali2001 <vaibhavmali2387@gmail.com>
2025-10-03 17:10:13 +0530

    restart

--- global_robotics_backend/global_robotics/src/main/java/com/site/Config/JwtUtil.java
+++ global_robotics_backend/global_robotics/src/main/java/com/site/Config/JwtUtil.java
@@ -22,14 +22,7 @@ public class JwtUtil {
         return secret;
     }
 
-//    public String generateToken(String username) {
-//        return Jwts.builder()
-//                .setSubject(username)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day expiration
-//                .signWith(SignatureAlgorithm.HS256, secret)
-//                .compact();
-//    } 
+
     
     public String generateToken(UserDetails userDetails) {
         String authorities = userDetails.getAuthorities().stream()
